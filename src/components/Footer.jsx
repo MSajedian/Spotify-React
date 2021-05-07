19ddcf9b863d305699a570d1f7d43b4284a08e64
MSajedian <mohammadsajedian@gmail.com>
2021-05-07 15:54:18 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,49 +7,49 @@ export default class Footer extends Component {
     }
     render() {
         return (
-            <div class="bottomMenuWrapper d-none d-sm-flex d-md-flex d-lg-flex">
-                <div class="bottomMenuWrapperLeft">
-                    <div class="d-flex">
+            <div className="bottomMenuWrapper d-none d-sm-flex d-md-flex d-lg-flex">
+                <div className="bottomMenuWrapperLeft">
+                    <div className="d-flex">
                         <img
                             src="Img/Highway 61.PNG"
-                            class="bottomMenuWrapperLeftAlbumImage"
+                            className="bottomMenuWrapperLeftAlbumImage"
                             alt=""
                         />
-                        <div class="bottomMenuWrapperLeftAlbumText">
+                        <div className="bottomMenuWrapperLeftAlbumText">
                             <h3>Song For My Father</h3>
                             <p>Horace Silver</p>
                         </div>
-                        <div class="bottomMenuWrapperLeftIcons">
+                        <div className="bottomMenuWrapperLeftIcons">
                             <ul>
-                                <li><i class="far fa-heart" aria-hidden="true"></i></li>
-                                <li><i class="fas fa-laptop" aria-hidden="true"></i></li>
+                                <li><i className="far fa-heart" aria-hidden="true"></i></li>
+                                <li><i className="fas fa-laptop" aria-hidden="true"></i></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div class="bottomMenuWrapperCenter">
+                <div className="bottomMenuWrapperCenter">
                     <div>
-                        <ul class="d-flex align-items-center justify-content-center">
-                            <li class="bottomMenuWrapperCenterSecondaryIcon">
-                                <i class="fas fa-random" aria-hidden="true"></i>
+                        <ul className="d-flex align-items-center justify-content-center">
+                            <li className="bottomMenuWrapperCenterSecondaryIcon">
+                                <i className="fas fa-random" aria-hidden="true"></i>
                             </li>
-                            <li class="bottomMenuWrapperCenterSecondaryIcon">
-                                <i class="fas fa-step-backward" aria-hidden="true"></i>
+                            <li className="bottomMenuWrapperCenterSecondaryIcon">
+                                <i className="fas fa-step-backward" aria-hidden="true"></i>
                             </li>
-                            <li class="bottomMenuWrapperCenterPlayIcon">
-                                <i class="far fa-play-circle" aria-hidden="true"></i>
+                            <li className="bottomMenuWrapperCenterPlayIcon">
+                                <i className="far fa-play-circle" aria-hidden="true"></i>
                             </li>
-                            <li class="bottomMenuWrapperCenterSecondaryIcon">
-                                <i class="fas fa-step-forward" aria-hidden="true"></i>
+                            <li className="bottomMenuWrapperCenterSecondaryIcon">
+                                <i className="fas fa-step-forward" aria-hidden="true"></i>
                             </li>
-                            <li class="bottomMenuWrapperCenterSecondaryIcon">
-                                <i class="fas fa-redo" aria-hidden="true"></i>
+                            <li className="bottomMenuWrapperCenterSecondaryIcon">
+                                <i className="fas fa-redo" aria-hidden="true"></i>
                             </li>
                         </ul>
                     </div>
-                    <div class="progress progressSong">
+                    <div className="progress progressSong">
                         <div
-                            class="progress-bar"
+                            className="progress-bar"
                             role="progressbar"
                             aria-valuenow="50"
                             aria-valuemin="0"
@@ -57,17 +57,17 @@ export default class Footer extends Component {
                         ></div>
                     </div>
                 </div>
-                <div class="bottomMenuWrapperRight d-flex align-items-center">
+                <div className="bottomMenuWrapperRight d-flex align-items-center">
                     <div>
                         <ul>
-                            <li><i class="fas fa-bars" aria-hidden="true"></i></li>
-                            <li><i class="fas fa-laptop-code" aria-hidden="true"></i></li>
+                            <li><i className="fas fa-bars" aria-hidden="true"></i></li>
+                            <li><i className="fas fa-laptop-code" aria-hidden="true"></i></li>
                             <li>
-                                <div class="d-flex align-items-center">
-                                    <i class="fas fa-volume-up" aria-hidden="true"></i>
-                                    <div class="progress progressVolume mb-0 ml-2">
+                                <div className="d-flex align-items-center">
+                                    <i className="fas fa-volume-up" aria-hidden="true"></i>
+                                    <div className="progress progressVolume mb-0 ml-2">
                                         <div
-                                            class="progress-bar mb-0"
+                                            className="progress-bar mb-0"
                                             role="progressbar"
                                             aria-valuenow="50"
                                             aria-valuemin="0"
@@ -78,7 +78,7 @@ export default class Footer extends Component {
                             </li>
                         </ul>
                         <i
-                            class="fas fa-ellipsis-h bottomMenuWrapperRightBars"
+                            className="fas fa-ellipsis-h bottomMenuWrapperRightBars"
                             aria-hidden="true"
                         ></i>
                     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,37 +8,37 @@ class Home extends Component {
     }
     render() {
         return (
-            <div class="contentSection pl-4 pr-1">
-                <div class="container pb-4 mb-5">
-                    <nav class="Page-navigation">
-                        <ul class="pagination justify-content-center">
-                            <li class="active page-item text-center">
-                                <a class="page-link" href="/"
+            <div className="contentSection pl-4 pr-1">
+                <div className="container pb-4 mb-5">
+                    <nav className="Page-navigation-home">
+                        <ul className="pagination justify-content-center">
+                            <li className="active page-item text-center">
+                                <a className="page-link" href="/"
                                 >TRENDING
-                <div class="link-borderBottom"></div
+                <div className="link-borderBottom"></div
                                     ></a>
                             </li>
-                            <li class="page-item text-center">
-                                <a class="page-link" href="/" >
-                                    <div class="link-borderBottom">
+                            <li className="page-item text-center">
+                                <a className="page-link" href="/" >
+                                    <div className="link-borderBottom">
                                     </div> PODCAST
                                 </a >
                             </li>
-                            <li class="page-item text-center">
-                                <a class="page-link" href="/" >
-                                    <div class="link-borderBottom">
+                            <li className="page-item text-center">
+                                <a className="page-link" href="/" >
+                                    <div className="link-borderBottom">
                                     </div> MOODS AND GENRES
                                         </a>
                             </li>
-                            <li class="page-item text-center">
-                                <a class="page-link" href="/" >
-                                    <div class="link-borderBottom">
+                            <li className="page-item text-center">
+                                <a className="page-link" href="/" >
+                                    <div className="link-borderBottom">
                                     </div> NEW RELEASES
                                         </a >
                             </li>
-                            <li class="page-item text-center">
-                                <a class="page-link" href="/" >
-                                    <div class="link-borderBottom">
+                            <li className="page-item text-center">
+                                <a className="page-link" href="/" >
+                                    <div className="link-borderBottom">
                                     </div> DISCOVER
                                         </a >
                             </li>
@@ -46,94 +46,94 @@ class Home extends Component {
                     </nav>
                 </div>
 
-                <div class="container mb-5">
-                    <h3 class="marginTop titleMobileHomepage">#THROWBACKTHURSDAY</h3>
-                    <div class="row mb-3 text-center">
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                <div className="container mb-5">
+                    <h3 className="marginTop titleMobileHomepage">#THROWBACKTHURSDAY</h3>
+                    <div className="row mb-3 text-center">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Italian Karaoke.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Italian Karaoke</p>
+                            <p className="mt-2 font-weight-bold">Italian Karaoke</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Lyricists.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Lyricists</p>
+                            <p className="mt-2 font-weight-bold">Lyricists</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Italy's frequent rotation.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Italy's Frequent Rotation</p>
+                            <p className="mt-2 font-weight-bold">Italy's Frequent Rotation</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/00s Italy.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">00s Italy</p>
+                            <p className="mt-2 font-weight-bold">00s Italy</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Cocktail Hour.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Cocktail Hour</p>
+                            <p className="mt-2 font-weight-bold">Cocktail Hour</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/I Love My '90s Hip-Hop.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">I Love My '90s Hip-Hop</p>
+                            <p className="mt-2 font-weight-bold">I Love My '90s Hip-Hop</p>
                         </div>
                     </div>
 
 
 
-                    <h3>Classifiche</h3>
-                    <div class="row p-0 text-center">
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                    <h3>classNameifiche</h3>
+                    <div className="row p-0 text-center">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Top 50 - Italy.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Top 50 - Italy</p>
+                            <p className="mt-2 font-weight-bold">Top 50 - Italy</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Top 50 - Global.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Top 50 - Global</p>
+                            <p className="mt-2 font-weight-bold">Top 50 - Global</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Viral 50 - Global.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Viral 50 - Global</p>
+                            <p className="mt-2 font-weight-bold">Viral 50 - Global</p>
                         </div>
-                        <div class="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-2 p-2">
                             <img
                                 src="Img/Viral 50 - Italy.jpg"
-                                class="img-fluid mb-1 homePageImage"
+                                className="img-fluid mb-1 homePageImage"
                                 alt=""
                             />
-                            <p class="mt-2 font-weight-bold">Viral 50 - Italy</p>
+                            <p className="mt-2 font-weight-bold">Viral 50 - Italy</p>
                         </div>
                     </div>
                 </div>
